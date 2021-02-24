61dd1238a653730e6b1d44ae76bc0925d0deb4a3
Smith <smith.mugalu@andela.com>
2021-02-24 10:31:14 +0300

    first commit with project files

---
+++ RoadTrippy/RoadTrippy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoadTrippy
+//
+//  Created by Smithers on 24/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RoadTrippy/RoadTrippy/RoadTrippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadTrippyApp.swift
+//  RoadTrippy
+//
+//  Created by Smithers on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct RoadTrippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
